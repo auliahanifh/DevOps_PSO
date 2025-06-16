789622c224b03b627c9c70da492d3faa355338e1
Aulia Hanifah <138230136+auliahanifh@users.noreply.github.com>
2025-06-16 21:23:24 +0700

    fix: seed database and import .env

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo "Running migrations..."
 php artisan migrate:fresh --force
 
 echo "Seeding database..."
-php artisan db:seed
+php artisan db:seed --class=UserSeeder
 
 echo "Import Keranjang Belanja"
 mysql -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" $DB_DATABASE < /var/www/html/trial_laravel.sql
